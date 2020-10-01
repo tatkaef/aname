8cdeb109d24aababb7b20a90184d05d0d90500f1
Tatiana Efimova <tatka_ef@mail.ru>
2020-10-01 11:03:24 -0700

    just commit

--- api/server.js
+++ api/server.js
@@ -3,6 +3,6 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ hello: "node 34 WOW! IT'S WORK" });
+  res.status(200).json({ hello: "node 34 WOW! IT'S WORKING!!!!!" });
 });
 module.exports = server;
